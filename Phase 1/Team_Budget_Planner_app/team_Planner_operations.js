ecbddc6b84b09280559d50f62a9255be2b7cc683
munjaap uppal <muppal@oakland.edu>
2021-08-13 23:42:58 -0400

    modified file

--- Phase 1/Team_Budget_Planner_app/team_Planner_operations.js
+++ Phase 1/Team_Budget_Planner_app/team_Planner_operations.js
@@ -1,65 +1,54 @@
 
-   function display() {
-    
-    // take value from text field using id or name
-    //we can store json object. but we have to convert 
-    // into string. 
-    
-    //let clientName = JSON.parse(localStorage.getItem("Client") || "[]");
-    
-    getData();
-    var myTable = document.getElementById("storageTable");
+var array = [];
+var array2 = [];
 
-    for(i=0;i<array.length;i++) {
-          var row = myTable.insertRow();
-          var x= r.insertCell();
-          
+function storeData() {
 
+    const table = localStorage.getItem("input");
+    if (table === null) {
+        array = [];
+    }else {
+        array2 = JSON.parse(table);
     }
+    var clientName=document.getElementById("Client").value;
+    var projectName=document.getElementById("Project").value;
+    var budget=document.getElementById("Budget").value;
 
-    var startTable ="<table border=1><tr><th>Id</th><th>Name</th><th>Age</th></tr>"
-    clientName.forEach(element => {tableContent ="<tr><td>"+clientName.id+"</td><td>"+projectName.name+"</td><td>"+budget.age+"</td></tr>"});
-    var endTable = "</table>";
-    tableContent = startTable + tableContent + endTable;
+
+    var value = {client: clientName, p: projectName, b: budget,}
+    array.push( value);
+
+        localStorage.setItem("input", JSON.stringify(array));
+        array.push.apply(array, array2);
+}
+
+
+function display() {
+    
+    
+    let input = localStorage.getItem("input");
+    let inputJson = JSON.parse(input);
+    var tableContent = "";
+    var sumOfbudget = 0;
+    var startTable ="<table border=1><tr><th>Client</th><th>Project Name</th><th>Budget</th></tr>"
+    for(var i=0; i<inputJson.length; i++) {
+        tableContent +="<tr><td>"+inputJson[i].client+"</td><td>"+inputJson[i].p +"</td><td>"+"$"+ inputJson[i].b + "</td></tr>"
+        sumOfbudget += Number(inputJson[i].b); 
+    }
+    tableContent +="<tr><td>"+"Total Budget"+"</td><td>"+"</td><td>"+"$"+sumOfbudget+"</td></tr>"
+  
+    var endTable="</table>"
+    tableContent = startTable+tableContent+endTable
     document.getElementById("table").innerHTML=tableContent;
- 
 } 
-   
-   
-   
-   
-   var array = new Array();
-    function addData(){
-        getData();
-        array.push({
-            Client: document.getElementById("Client").value,
-            Project: document.getElementById("Project").value,
-            Budget: document.getElementById("Budget").value,
-        });
-        localStorage.setItem("storageData", JSON.stringify(array));
-        display();
-    }
-    function getData(){
-        var table = localStorage.getItem("storageData");
-        if (table != null)
-            array = JSON.parse(table);
-    }
+
+
+function deleteEntry(){
+        localStorage.removeItem('input');
+       // alert("Cleared all data");
     
-    function storeData() {
-        clientName=document.getElementById("Client").value;
-        projectName=document.getElementById("Project").value;
-        budget=document.getElementById("Budget").value;
-    }
-
-    /* var startTable ="<table border=1><tr><th>Id</th><th>Name</th><th>Age</th></tr>"
-    
-    tableContent ="<tr><td>"+empJson.id+"</td><td>"+empJson.name+"</td><td>"+empJson.age+"</td></tr>"
-
-clientName=document.getElementById(client).value;
-
-let projectName = JSON.parse(localStorage.getItem("Project") || "[]");
-        var tableContent = "";
-    
-        companies.push(company);
-        localStorage.setItem("companies", JSON.stringify(companies)) */
-
+}
+   
+   
+   
+ 
